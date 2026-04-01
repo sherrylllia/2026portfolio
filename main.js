779eb988400d5f65e1e10d6afc02156b6f35ff57
sherrylllia <sherry200493@gmail.com>
2026-04-01 09:09:20 +0800

    first version of my portfolio

---
+++ main.js
@@ -0,0 +1,10 @@
+// Scroll reveal animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((e, i) => {
+    if (e.isIntersecting) {
+      setTimeout(() => e.target.classList.add('visible'), i * 80);
+    }
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
